1e67d622ba76e06bccbea1661b85365fc6c08e3d
Naveen Bellary <bellary.naveen@gmail.com>
2013-06-27 11:22:31 +0530

    firsttry

--- src/AmConstants.java
+++
@@ -1,6 +0,0 @@
-
-public class AmConstants {
-    public final static int millWorkingHours= 8;
-    public static final int numberOfPulses=5;
-    public static final long daysToMillisecs=24*60*60*100;
-}
