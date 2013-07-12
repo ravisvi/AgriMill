85d7f9b6ae3a13080522fa64483c3cec08146dfb
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2013-07-12 11:07:13 +0530

    Modified random electric delay
    Random electric delay has been modified to electric delay based upon
    probability distribution as documented.
    **
    the existing method 'int generateByProbability(int pulseNumber)' has
    been changed to 'int generatePulseByProbability(int pulseNumber)'
    **

--- src/AmUtils.java
+++ src/AmUtils.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 public class AmUtils {
 	static Random random = new Random();
-	static int generateByProbablity(int pulseNumber) {
-		
+	static int generatePulseByProbablity(int pulseNumber) {
+
 		int[] distribution = {5, 8, 10}; 
-		/* probability distribution array
+		/* probability distribution array for pulse generation
 		 * 50% for 1-15 kg
 		 * 30% for 16-40 kg
 		 * 20% for 40+ kg
@@ -22,5 +22,28 @@ public class AmUtils {
 		else {
 			return 0; //ideally should never come here
 		}			
+	}
+
+	static int generateElectricityCutByProbablity() {
+
+		int[] distribution = {2, 8, 10}; 
+		/* probability distribution array for electricity cut
+		 * 20% for 0 hours (no electricity cut that day)
+		 * 60% for 1-3 hours
+		 * 20% for 4-10 hours
+		 */
+		int randomKey = 1 + random.nextInt(9);
+		if (randomKey <= distribution[0]) {
+			return 0;
+		} 
+		else if(randomKey > distribution[0] && randomKey <= distribution[1]){
+			return (1 + random.nextInt(3));
+		}
+		else if (randomKey > distribution[1] && randomKey <= distribution[2]) {
+			return (5 + random.nextInt(11));
+		}
+		else {
+			return 0; //ideally should never come here
+		}		
 	}
 }
--- src/Mill.java
+++ src/Mill.java
@@ -3,72 +3,72 @@ import java.sql.Statement;
 
 
 public class Mill{
-    //Contains 5 pulses for rice, wheat corn millet and ragi
-    public Pulse[] pulses = new Pulse[AmConstants.numberOfPulses];
-    String[] name = {"Rice", "Wheat", "Rawa", "Millet", "Corn"};
+	//Contains 5 pulses for rice, wheat corn millet and ragi
+	public Pulse[] pulses = new Pulse[AmConstants.numberOfPulses];
+	String[] name = {"Rice", "Wheat", "Rawa", "Millet", "Corn"};
 	private int timeTaken;
 
-    public Mill(){
-        for(int pulseCount = 0; pulseCount < AmConstants.numberOfPulses; pulseCount++){
-        pulses[pulseCount] = new Pulse(this.name[pulseCount]);
-        }
-    }
+	public Mill(){
+		for(int pulseCount = 0; pulseCount < AmConstants.numberOfPulses; pulseCount++){
+			pulses[pulseCount] = new Pulse(this.name[pulseCount]);
+		}
+	}
 
-    public void generate(int millNumber,Connect connector, Thread thread)
-    {
-        //put db insertion here.
-        int numberOfWorkingHours=AmConstants.millWorkingHours;
-        int pulseCount;
-        int tempConsumption[]=new int[AmConstants.numberOfPulses];
-        
-      //Adding minimum consumption first.
-        for(int randomQty=0;randomQty<5;randomQty++){
-        	int getQuantity=AmUtils.random.nextInt(10)+1;
-        	tempConsumption[randomQty]=getQuantity;
-        	numberOfWorkingHours-=(int)((getQuantity/AmConstants.pulseProductionTime[randomQty]));
-         }
-      	
-    	int randomElectricDelay=AmUtils.random.nextInt(2);
-        //Add a random Electric Delay because of power cut
-    	numberOfWorkingHours=reduceTime(randomElectricDelay, numberOfWorkingHours, thread);
-        while(numberOfWorkingHours>0){
-            pulseCount=AmUtils.random.nextInt(AmConstants.numberOfPulses);
-            //Get a random pulse.
-            
-          
-            int randomQuantity = AmUtils.generateByProbablity(pulseCount);
-            this.pulses[pulseCount].addConsumptionBy(randomQuantity);
-            
-            //Add a random consumption quantity to that pulse.
+	public void generate(int millNumber,Connect connector, Thread thread)
+	{
+		//put db insertion here.
+		int numberOfWorkingHours=AmConstants.millWorkingHours;
+		int pulseCount;
+		int tempConsumption[]=new int[AmConstants.numberOfPulses];
 
-            tempConsumption[pulseCount]+=randomQuantity;
-            if(pulseCount==0){
-                timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[0]);
-                numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread); //rice = 100/hour
-            }
+		//Adding minimum consumption first.
+		for(int randomQty=0;randomQty<5;randomQty++){
+			int getQuantity=AmUtils.random.nextInt(10)+1;
+			tempConsumption[randomQty]=getQuantity;
+			numberOfWorkingHours-=(int)((getQuantity/AmConstants.pulseProductionTime[randomQty]));
+		}
 
-            else if(pulseCount==1){
-            	timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[1]);
-                numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);//wheat = 100/hour
-            }
+		int randomElectricDelay = AmUtils.generateElectricityCutByProbablity();
+		//Add a random Electric Delay because of power cut
+		numberOfWorkingHours = reduceTime(randomElectricDelay, numberOfWorkingHours, thread);
+		while(numberOfWorkingHours>0){
+			pulseCount=AmUtils.random.nextInt(AmConstants.numberOfPulses);
+			//Get a random pulse.
 
-            else if(pulseCount==2){
-            	timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[2]);
-                numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);//rawa = 200/hour
-            }
 
-            else if(pulseCount==3){
-            	timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[3]);
-                numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);  //millet = 70/hour
-            }
+			int randomQuantity = AmUtils.generatePulseByProbablity(pulseCount);
+			this.pulses[pulseCount].addConsumptionBy(randomQuantity);
 
-            else{
-            	timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[4]);
-                numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);   //ragi = 200/hour
-            }
+			//Add a random consumption quantity to that pulse.
 
-        }
-        try {
+			tempConsumption[pulseCount]+=randomQuantity;
+			if(pulseCount==0){
+				timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[0]);
+				numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread); //rice = 100/hour
+			}
+
+			else if(pulseCount==1){
+				timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[1]);
+				numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);//wheat = 100/hour
+			}
+
+			else if(pulseCount==2){
+				timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[2]);
+				numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);//rawa = 200/hour
+			}
+
+			else if(pulseCount==3){
+				timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[3]);
+				numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);  //millet = 70/hour
+			}
+
+			else{
+				timeTaken = (int)((this.pulses[pulseCount].getConsumptionQty())/AmConstants.pulseProductionTime[4]);
+				numberOfWorkingHours=reduceTime(timeTaken, numberOfWorkingHours, thread);   //ragi = 200/hour
+			}
+
+		}
+		try {
 			Statement statement=connector.conn.createStatement();
 			String insertion="INSERT INTO mill"+millNumber+" values("+tempConsumption[0]+","+tempConsumption[1]+","+tempConsumption[2]+","+tempConsumption[3]+","+tempConsumption[4]+");";
 			statement.executeUpdate(insertion);
@@ -77,17 +77,17 @@ public class Mill{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-    }
-    public int reduceTime(int timeTaken,int numberOfWorkingHours, Thread thread){
-    	numberOfWorkingHours-=timeTaken; 
-        try {
+
+	}
+	public int reduceTime(int timeTaken,int numberOfWorkingHours, Thread thread){
+		numberOfWorkingHours-=timeTaken; 
+		try {
 			thread.sleep(timeTaken*AmConstants.hoursToMillisecs);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-    	return numberOfWorkingHours;
-    }
+
+		return numberOfWorkingHours;
+	}
 }
